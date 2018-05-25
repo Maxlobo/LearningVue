051c124f0133fc617eb966675e646c9e4b107ffe
Max <maxhb.df@gmail.com>
2018-05-24 21:17:00 -0300

    Inicio.

---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+import Test from './components/test'
+
+export default{
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
